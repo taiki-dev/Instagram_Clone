388a148d95639f9d84b12463972312adebf306aa
taiki <taiki.murakoshi.0908@gmail.com>
2021-02-25 18:42:22 +0900

    もう少し進んだ

--- accounts/views.py
+++ accounts/views.py
@@ -7,3 +7,9 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+def index(request):
+    context = {
+        'users':request.user,
+    }
+    return render(request, 'index.html', context)

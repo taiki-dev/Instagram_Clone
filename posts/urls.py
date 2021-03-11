e6a013d9938c4682f18b5ed5cc75615e80f7437e
taiki-dev <taiki.murakoshi.0908@gmail.com>
2021-03-11 15:12:56 +0900

    ホーム画面（index.html)に投稿を表示できるように設定，かつほかのユーザーでも見れるようにした

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from .views import FormClass, ListClass
 
 urlpatterns = [
     path('form/', FormClass.as_view(), name='form'),
-    path('list/', ListClass.as_view(), name='list'),
+    path('index/', ListClass.as_view(), name='index'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,11 +5,11 @@ from django.urls import reverse_lazy
 from .models import PostModel
 
 class ListClass(ListView):
-    template_name = 'list.html'
+    template_name = 'index.html'
     model = PostModel
 
 class FormClass(CreateView):
     template_name = 'form.html'
     model = PostModel
     fields = ('title', 'memo')
-    success_url = reverse_lazy('list')
+    success_url = reverse_lazy('index')

de0c6dee6ec22c001bbef73efca84a85101a3ec6
taiki-dev <taiki.murakoshi.0908@gmail.com>
2021-03-11 16:21:07 +0900

    ログインしないと投稿できないようにした

--- posts/views.py
+++ posts/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
 from .models import PostModel
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class ListClass(ListView):
     template_name = 'index.html'
     model = PostModel
 
-class FormClass(CreateView):
+class FormClass(LoginRequiredMixin, CreateView):
     template_name = 'form.html'
     model = PostModel
     fields = ('title', 'memo')
